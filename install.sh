8d69efff0bcc76606303bc0fa40b034b285456ba
Paul Reiter <reiter.paul@gmail.com>
2019-04-07 21:25:41 +0200

    set file permissions of baku

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 mkdir -p /usr/local/man/man1/
-install -g 0 -o 0 -m 0644 baku /usr/local/bin/
+install -g 0 -o 0 -m 0755 baku /usr/local/bin/
 install -g 0 -o 0 -m 0644 baku.1 /usr/local/man/man1/
 gzip /usr/local/man/man1/baku.1
